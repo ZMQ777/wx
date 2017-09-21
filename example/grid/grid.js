2dd97b48b2be3bc8526eb0939ee3e6a8b0ea189d
zmq777 <m18694057205@163.com>
2017-09-21 11:12:37 +0800

    1

--- example/grid/grid.js
+++ example/grid/grid.js
@@ -1,5 +1,32 @@
 Page({
     data: {
-        grids: [0, 1, 2, 3, 4, 5, 6, 7, 8]
+      grids: [
+        {
+          label: '1',
+          src: '../../../images/bar_one.jpg',
+          url: '../views/list3/list3?t=1'
+        }, {
+          label: '2',
+          src: '../../../images/bar_two.jpg',
+          url: '../views/list3/list3?t=2'
+        }, {
+          label: '3',
+          src: '../../../images/bar_three.jpg',
+          url: '../views/list3/list3?t=3'
+        }
+        , {
+          label: '4',
+          src: '../../../images/bar_four.jpg',
+          url: '../views/list3/list3?t=4'
+        }, {
+          label: '5',
+          src: '../../../images/bar_five.jpg',
+          url: '../views/list3/list3?t=5'
+        }, {
+          label: '6',
+          src: '../../../images/bar_six.jpg',
+          url: '../views/list3/list3?t=6'
+        }
+      ]
     }
 });
