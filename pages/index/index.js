7f13103a5b9a4d7dfc2801e796202795f407c118
zmq777 <m18694057205@163.com>
2017-09-21 11:52:36 +0800

    init2

--- pages/index/index.js
+++ pages/index/index.js
@@ -7,28 +7,28 @@ Page({
     grids: [
       {
         label: '1',
-        src: '../../../images/bar_one.jpg',
+        src: '../../../static/img/bar_one.jpg',
         url: '../views/list3/list3?t=1'
       }, {
         label: '2',
-        src: '../../../images/bar_two.jpg',
+        src: '../../../static/img/bar_two.jpg',
         url: '../views/list3/list3?t=2'
       }, {
         label: '3',
-        src: '../../../images/bar_three.jpg',
+        src: '../../../static/img/bar_three.jpg',
         url: '../views/list3/list3?t=3'
       }
       , {
         label: '4',
-        src: '../../../images/bar_four.jpg',
+        src: '../../../static/img/bar_four.jpg',
         url: '../views/list3/list3?t=4'
       }, {
         label: '5',
-        src: '../../../images/bar_five.jpg',
+        src: '../../../static/img/bar_five.jpg',
         url: '../views/list3/list3?t=5'
       }, {
         label: '6',
-        src: '../../../images/bar_six.jpg',
+        src: '../../../static/img/bar_six.jpg',
         url: '../views/list3/list3?t=6'
       }
     ],
