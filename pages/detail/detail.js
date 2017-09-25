aaaac456c01d8e27467eae869dfc6321e84ebd25
hua03 <295506287@qq.com>
2017-09-25 10:58:32 +0800

    详情页

--- pages/detail/detail.js
+++ pages/detail/detail.js
@@ -1,85 +1,352 @@
 Page({
   data: {
-    goodsBanner: [
-      'http://odwbo6hwu.bkt.clouddn.com/webapp/img/detail/pifu/visia/good-banner.jpg',
-      'http://odwbo6hwu.bkt.clouddn.com/webapp/img/detail/pifu/visia/good-banner.jpg',
-      'http://odwbo6hwu.bkt.clouddn.com/webapp/img/detail/pifu/visia/good-banner.jpg'
-    ],
-    goodsName: "【VISIA皮肤检测】精确的皮肤分析",
-    price: "9.00",
-    originalPrice: "50",
-    fee: "免运费",
-    amount: "100",
-    sales: "783",
-    goodsDetailImg: [
-      "http://odwbo6hwu.bkt.clouddn.com/webapp/img/detail/pifu/visia/good-detail.jpg",
-      "http://odwbo6hwu.bkt.clouddn.com/webapp/img/detail/pifu/visia/good-detail.jpg"
-    ],
-    hotGoods: [
-      [{
-        text: '',
-        src: 'http://odwbo6hwu.bkt.clouddn.com/webapp/img/list/pifu/pifu_01.jpg',
-        url: '/pages/detail/detail',
-        parse: '11.00',
-        discount_price: ''
-      }, {
-        text: '',
-        src: 'http://odwbo6hwu.bkt.clouddn.com/webapp/img/list/pifu/pifu_02.jpg',
-        url: '/pages/detail/detail',
-        parse: '11.00',
-        discount_price: ''
-      }, {
-        text: '',
-        src: 'http://odwbo6hwu.bkt.clouddn.com/webapp/img/list/pifu/pifu_03.jpg',
-        url: '/pages/detail/detail',
-        parse: '110',
-        discount_price: '1000'
-      }, {
-        text: '',
-        src: 'http://odwbo6hwu.bkt.clouddn.com/webapp/img/list/pifu/pifu_04.jpg',
-        url: '/pages/detail/detail',
-        parse: '300',
-        discount_price: '3000'
-      }],
-      [{
-        text: '',
-        src: 'http://odwbo6hwu.bkt.clouddn.com/webapp/img/list/pifu/pifu_02.jpg',
-        url: '/pages/detail/detail',
-        parse: '11.00',
-        discount_price: ''
-      }, {
-        text: '',
-        src: 'http://odwbo6hwu.bkt.clouddn.com/webapp/img/list/pifu/pifu_03.jpg',
-        url: '/pages/detail/detail',
-        parse: '110',
-        discount_price: '1000'
-      }, {
-        text: '',
-        src: 'http://odwbo6hwu.bkt.clouddn.com/webapp/img/list/pifu/pifu_04.jpg',
-        url: '/pages/detail/detail',
-        parse: '300',
-        discount_price: '3000'
-      }]
-    ]
+    pageGoodInfo: null,
+    pageHotGood: null,
+    
+    goods: {
+      VISIApifujiance:{
+        goodsBanner: [
+          'http://odwbo6hwu.bkt.clouddn.com/webapp/img/detail/pifu/visia/good-banner.jpg',
+          'http://odwbo6hwu.bkt.clouddn.com/webapp/img/detail/pifu/visia/good-banner.jpg',
+          'http://odwbo6hwu.bkt.clouddn.com/webapp/img/detail/pifu/visia/good-banner.jpg'
+        ],
+        goodsName: "【VISIA皮肤检测】精确的皮肤分析",
+        price: "9.00",
+        price2: "50",
+        fee: "免运费",
+        amount: "100",
+        sales: "783",
+        goodsDetailImg: [
+          "http://odwbo6hwu.bkt.clouddn.com/webapp/img/detail/pifu/visia/good-detail.jpg",
+          "http://odwbo6hwu.bkt.clouddn.com/webapp/img/detail/pifu/visia/good-detail.jpg"
+        ]
+      }
+    },
+    hotGoods: {
+      pifu: [
+        {
+          text: '',
+          src: 'http://odwbo6hwu.bkt.clouddn.com/webapp/img/list/pifu/pifu_01.jpg',
+          url: '/pages/detail/detail?list=pifu&project=VISIApifujiance',
+          parse: '11.00',
+          discount_price: ''
+        },
+        {
+          text: '',
+          src: 'http://odwbo6hwu.bkt.clouddn.com/webapp/img/list/pifu/pifu_02.jpg',
+          url: '/pages/detail/detail?list=pifu&project=yiyongmianmo',
+          parse: '11.00',
+          discount_price: ''
+        },
+        {
+          text: '',
+          src: 'http://odwbo6hwu.bkt.clouddn.com/webapp/img/list/pifu/pifu_03.jpg',
+          url: '/pages/detail/detail?list=pifu&project=shuiyanghuofu',
+          parse: '110',
+          discount_price: '1000'
+        },
+        {
+          text: '',
+          src: 'http://odwbo6hwu.bkt.clouddn.com/webapp/img/list/pifu/pifu_04.jpg',
+          url: '/pages/detail/detail?list=pifu&project=weizhenmeisu',
+          parse: '300',
+          discount_price: '3000'
+        },
+        {
+          text: '',
+          src: 'http://odwbo6hwu.bkt.clouddn.com/webapp/img/list/pifu/pifu_05.jpg',
+          url: '/pages/detail/detail?list=pifu&project=zhongxifadou',
+          parse: '160',
+          discount_price: '580'
+        },
+        {
+          text: '',
+          src: 'http://odwbo6hwu.bkt.clouddn.com/webapp/img/list/pifu/pifu_06.jpg',
+          url: '/pages/detail/detail?list=pifu&project=qudouVISIAshuiyangsanheyi',
+          parse: '380',
+          discount_price: '2080'
+        },
+        {
+          text: '',
+          src: 'http://odwbo6hwu.bkt.clouddn.com/webapp/img/list/pifu/pifu_07.jpg',
+          url: '/pages/detail/detail?list=pifu&project=Eguangmeibainenfu',
+          parse: '360',
+          discount_price: '3200'
+        },
+        {
+          text: '',
+          src: 'http://odwbo6hwu.bkt.clouddn.com/webapp/img/list/pifu/pifu_08.jpg',
+          url: '/pages/detail/detail?list=pifu&project=Eguangshuiyang',
+          parse: '380',
+          discount_price: '2080'
+        },
+        {
+          text: '',
+          src: 'http://odwbo6hwu.bkt.clouddn.com/webapp/img/list/pifu/pifu_09.jpg',
+          url: '/pages/detail/detail?list=pifu&project=tuofajiancha',
+          parse: '11.00',
+          discount_price: '30'
+        },
+        {
+          text: '',
+          src: 'http://odwbo6hwu.bkt.clouddn.com/webapp/img/list/pifu/pifu_10.jpg',
+          url: '/pages/detail/detail?list=pifu&project=pifujiancha',
+          parse: '25.00',
+          discount_price: '60'
+        },
+        {
+          text: '',
+          src: 'http://odwbo6hwu.bkt.clouddn.com/webapp/img/list/pifu/pifu_11.jpg',
+          url: '/pages/detail/detail?list=pifu&project=huizhijiajiancha',
+          parse: '11.00',
+          discount_price: '30'
+        },
+        {
+          text: '',
+          src: 'http://odwbo6hwu.bkt.clouddn.com/webapp/img/list/pifu/pifu_12.jpg',
+          url: '/pages/detail/detail?list=pifu&project=guomingyuanjiance',
+          parse: '240',
+          discount_price: '480'
+        },
+        {
+          text: '',
+          src: 'http://odwbo6hwu.bkt.clouddn.com/webapp/img/list/pifu/pifu_13.jpg',
+          url: '/pages/detail/detail?list=pifu&project=yechoushoushu',
+          parse: '280',
+          discount_price: '3000'
+        },
+        {
+          text: '',
+          src: 'http://odwbo6hwu.bkt.clouddn.com/webapp/img/list/pifu/pifu_14.jpg',
+          url: '/pages/detail/detail?list=pifu&project=qiangxiaoqudou',
+          parse: '5.00',
+          discount_price: ''
+        }
+      ],
+      fuke:[
+        {
+          text: '',
+          src: 'http://odwbo6hwu.bkt.clouddn.com/webapp/img/list/fuke/fuke_01.jpg',
+          url: '/pages/detail/detail?list=women&project=women5',
+          parse: '60',
+          discount_price: '300'
+        }, {
+          text: '',
+          src: 'http://odwbo6hwu.bkt.clouddn.com/webapp/img/list/fuke/fuke_02.jpg',
+          url: '/pages/detail/detail?list=women&project=women6',
+          parse: '100',
+          discount_price: '340'
+        }, {
+          text: '',
+          src: 'http://odwbo6hwu.bkt.clouddn.com/webapp/img/list/fuke/fuke_03.jpg',
+          url: '/pages/detail/detail?list=women&project=women1',
+          parse: '100',
+          discount_price: '200'
+        }, {
+          text: '',
+          src: 'http://odwbo6hwu.bkt.clouddn.com/webapp/img/list/fuke/fuke_04.jpg',
+          url: '/pages/detail/detail?list=women&project=women2',
+          parse: '280',
+          discount_price: '562'
+        }, {
+          text: '',
+          src: 'http://odwbo6hwu.bkt.clouddn.com/webapp/img/list/fuke/fuke_05.jpg',
+          url: '/pages/detail/detail?list=women&project=women3',
+          parse: '320',
+          discount_price: '648'
+        }, {
+          text: '',
+          src: 'http://odwbo6hwu.bkt.clouddn.com/webapp/img/list/fuke/fuke_06.jpg',
+          url: '/pages/detail/detail?list=women&project=women4',
+          parse: '420',
+          discount_price: '848'
+        }
+      ],
+      nanke:[
+        {
+          text: '',
+          src: 'http://odwbo6hwu.bkt.clouddn.com/webapp/img/list/nanke/nanke_01.jpg',
+          url: '/pages/detail/detail?list=man&project=man1',
+          parse: '70',
+          discount_price: '140'
+        }, {
+          text: '',
+          src: 'http://odwbo6hwu.bkt.clouddn.com/webapp/img/list/nanke/nanke_02.jpg',
+          url: '/pages/detail/detail?list=man&project=man2',
+          parse: '240',
+          discount_price: '482'
+        },{
+          text: '',
+          src: 'http://odwbo6hwu.bkt.clouddn.com/webapp/img/list/nanke/nanke_03.jpg',
+          url: '/pages/detail/detail?list=man&project=man3',
+          parse: '320',
+          discount_price: '648'
+        },{
+          text: '',
+          src: 'http://odwbo6hwu.bkt.clouddn.com/webapp/img/list/nanke/nanke_04.jpg',
+          url: '/pages/detail/detail?list=man&project=man4',
+          parse: '420',
+          discount_price: '848'
+        }
+      ]
+    }
   },
-  changeIndicatorDots: function (e) {
-    this.setData({
-      indicatorDots: !this.data.indicatorDots
-    })
-  },
-  changeAutoplay: function (e) {
-    this.setData({
-      autoplay: !this.data.autoplay
-    })
-  },
-  intervalChange: function (e) {
-    this.setData({
-      interval: e.detail.value
-    })
-  },
-  durationChange: function (e) {
-    this.setData({
-      duration: e.detail.value
-    })
+  onLoad: function (options) {
+    var list = options.list
+    var project = options.project
+    if (list == "pifu") {
+      switch (project) {
+        case "Eguangmeibainenfu":
+          this.setData({
+            pageGoodInfo: this.data.goods.Eguangmeibainenfu,
+            pageHotGood: this.data.hotGoods.pifu
+          })
+          break;
+        case "Eguangshuiyang":
+          this.setData({
+            pageGoodInfo: this.data.goods.Eguangshuiyang,
+            pageHotGood: this.data.hotGoods.pifu
+          })
+          break;
+        case "guomingyuanjiance":
+          this.setData({
+            pageGoodInfo: this.data.goods.guomingyuanjiance,
+            pageHotGood: this.data.hotGoods.pifu
+          })
+          break;
+        case "huizhijiajiancha":
+          this.setData({
+            pageGoodInfo: this.data.goods.huizhijiajiancha,
+            pageHotGood: this.data.hotGoods.pifu
+          })
+          break;
+        case "pifujiancha":
+          this.setData({
+            pageGoodInfo: this.data.goods.pifujiancha,
+            pageHotGood: this.data.hotGoods.pifu
+          })
+          break;
+        case "qiangxiaoqudou":
+          this.setData({
+            pageGoodInfo: this.data.goods.qiangxiaoqudou,
+            pageHotGood: this.data.hotGoods.pifu
+          })
+          break;
+        case "qudouVISIAshuiyangsanheyi":
+          this.setData({
+            pageGoodInfo: this.data.goods.qudouVISIAshuiyangsanheyi,
+            pageHotGood: this.data.hotGoods.pifu
+          })
+          break;
+        case "shuiyanghuofu":
+          this.setData({
+            pageGoodInfo: this.data.goods.shuiyanghuofu,
+            pageHotGood: this.data.hotGoods.pifu
+          })
+          break;
+        case "tuofajiancha":
+          this.setData({
+            pageGoodInfo: this.data.goods.tuofajiancha,
+            pageHotGood: this.data.hotGoods.pifu
+          })
+          break;
+        case "VISIApifujiance":
+          this.setData({
+            pageGoodInfo: this.data.goods.VISIApifujiance,
+            pageHotGood: this.data.hotGoods.pifu
+          })
+          break;
+        case "weizhenmeisu":
+          this.setData({
+            pageGoodInfo: this.data.goods.weizhenmeisu,
+            pageHotGood: this.data.hotGoods.pifu
+          })
+          break;
+        case "yechoushoushu":
+          this.setData({
+            pageGoodInfo: this.data.goods.yechoushoushu,
+            pageHotGood: this.data.hotGoods.pifu
+          })
+          break;
+        case "yiyongmianmo":
+          this.setData({
+            pageGoodInfo: this.data.goods.yiyongmianmo,
+            pageHotGood: this.data.hotGoods.pifu
+          })
+          break;
+        case "zhongxifadou":
+          this.setData({
+            pageGoodInfo: this.data.goods.zhongxifadou,
+            pageHotGood: this.data.hotGoods.pifu
+          })
+          break;
+      } 
+    }else if(list == "man"){
+      switch (project){
+        case "man1":
+        this.setData({
+          pageGoodInfo: this.data.goods.man1,
+          pageHotGood: this.data.hotGoods.man
+        })
+        break;
+        case "man2":
+        this.setData({
+          pageGoodInfo: this.data.goods.man2,
+          pageHotGood: this.data.hotGoods.man
+        })
+        break;
+        case "man3":
+        this.setData({
+          pageGoodInfo: this.data.goods.man3,
+          pageHotGood: this.data.hotGoods.man
+        })
+        break;
+        case "man4":
+        this.setData({
+          pageGoodInfo: this.data.goods.man4,
+          pageHotGood: this.data.hotGoods.man
+        })
+        break;
+      }
+    }else if(list == "woman"){
+      switch (project){
+        case "woman1":
+        this.setData({
+          pageGoodInfo: this.data.goods.woman1,
+          pageHotGood: this.data.hotGoods.woman
+        })
+        break;
+        case "woman2":
+        this.setData({
+          pageGoodInfo: this.data.goods.woman2,
+          pageHotGood: this.data.hotGoods.woman
+        })
+        break;
+        case "woman3":
+        this.setData({
+          pageGoodInfo: this.data.goods.woman3,
+          pageHotGood: this.data.hotGoods.woman
+        })
+        break;
+        case "woman4":
+        this.setData({
+          pageGoodInfo: this.data.goods.woman4,
+          pageHotGood: this.data.hotGoods.woman
+        })
+        break;
+        case "woman5":
+        this.setData({
+          pageGoodInfo: this.data.goods.woman5,
+          pageHotGood: this.data.hotGoods.woman
+        })
+        break;
+        case "woman6":
+        this.setData({
+          pageGoodInfo: this.data.goods.woman6,
+          pageHotGood: this.data.hotGoods.woman
+        })
+        break;
+      }
+    }
   }
 })
--- pages/list/list.js
+++ pages/list/list.js
@@ -13,98 +13,98 @@ Page({
         {
           text: '',
           src: 'http://odwbo6hwu.bkt.clouddn.com/webapp/img/list/pifu/pifu_01.jpg',
-          url: '/pages/detail/detail',
+          url: '/pages/detail/detail?list=pifu&project=VISIApifujiance',
           parse: '11.00',
           discount_price: ''
         },
         {
           text: '',
           src: 'http://odwbo6hwu.bkt.clouddn.com/webapp/img/list/pifu/pifu_02.jpg',
-          url: '/pages/detail/detail',
+          url: '/pages/detail/detail?list=pifu&project=yiyongmianmo',
           parse: '11.00',
           discount_price: ''
         },
         {
           text: '',
           src: 'http://odwbo6hwu.bkt.clouddn.com/webapp/img/list/pifu/pifu_03.jpg',
-          url: '/pages/detail/detail',
+          url: '/pages/detail/detail?list=pifu&project=shuiyanghuofu',
           parse: '110',
           discount_price: '1000'
         },
         {
           text: '',
           src: 'http://odwbo6hwu.bkt.clouddn.com/webapp/img/list/pifu/pifu_04.jpg',
-          url: '/pages/detail/detail',
+          url: '/pages/detail/detail?list=pifu&project=weizhenmeisu',
           parse: '300',
           discount_price: '3000'
         },
         {
           text: '',
           src: 'http://odwbo6hwu.bkt.clouddn.com/webapp/img/list/pifu/pifu_05.jpg',
-          url: '/pages/detail/detail',
+          url: '/pages/detail/detail?list=pifu&project=zhongxifadou',
           parse: '160',
           discount_price: '580'
         },
         {
           text: '',
           src: 'http://odwbo6hwu.bkt.clouddn.com/webapp/img/list/pifu/pifu_06.jpg',
-          url: '/pages/detail/detail',
+          url: '/pages/detail/detail?list=pifu&project=qudouVISIAshuiyangsanheyi',
           parse: '380',
           discount_price: '2080'
         },
         {
           text: '',
           src: 'http://odwbo6hwu.bkt.clouddn.com/webapp/img/list/pifu/pifu_07.jpg',
-          url: '/pages/detail/detail',
+          url: '/pages/detail/detail?list=pifu&project=Eguangmeibainenfu',
           parse: '360',
           discount_price: '3200'
         },
         {
           text: '',
           src: 'http://odwbo6hwu.bkt.clouddn.com/webapp/img/list/pifu/pifu_08.jpg',
-          url: '/pages/detail/detail',
+          url: '/pages/detail/detail?list=pifu&project=Eguangshuiyang',
           parse: '380',
           discount_price: '2080'
         },
         {
           text: '',
           src: 'http://odwbo6hwu.bkt.clouddn.com/webapp/img/list/pifu/pifu_09.jpg',
-          url: '/pages/detail/detail',
+          url: '/pages/detail/detail?list=pifu&project=tuofajiancha',
           parse: '11.00',
           discount_price: '30'
         },
         {
           text: '',
           src: 'http://odwbo6hwu.bkt.clouddn.com/webapp/img/list/pifu/pifu_10.jpg',
-          url: '/pages/detail/detail',
+          url: '/pages/detail/detail?list=pifu&project=pifujiancha',
           parse: '25.00',
           discount_price: '60'
         },
         {
           text: '',
           src: 'http://odwbo6hwu.bkt.clouddn.com/webapp/img/list/pifu/pifu_11.jpg',
-          url: '/pages/detail/detail',
+          url: '/pages/detail/detail?list=pifu&project=huizhijiajiancha',
           parse: '11.00',
           discount_price: '30'
         },
         {
           text: '',
           src: 'http://odwbo6hwu.bkt.clouddn.com/webapp/img/list/pifu/pifu_12.jpg',
-          url: '/pages/detail/detail',
+          url: '/pages/detail/detail?list=pifu&project=guomingyuanjiance',
           parse: '240',
           discount_price: '480'
         },
         {
           text: '',
           src: 'http://odwbo6hwu.bkt.clouddn.com/webapp/img/list/pifu/pifu_13.jpg',
-          url: '/pages/detail/detail',
+          url: '/pages/detail/detail?list=pifu&project=yechoushoushu',
           parse: '280',
           discount_price: '3000'
         },
         {
           text: '',
           src: 'http://odwbo6hwu.bkt.clouddn.com/webapp/img/list/pifu/pifu_14.jpg',
-          url: '/pages/detail/detail',
+          url: '/pages/detail/detail?list=pifu&project=qiangxiaoqudou',
           parse: '5.00',
           discount_price: ''
         }
@@ -113,37 +113,37 @@ Page({
         {
           text: '',
           src: 'http://odwbo6hwu.bkt.clouddn.com/webapp/img/list/fuke/fuke_01.jpg',
-          url: '',
+          url: '/pages/detail/detail?list=women&project=women5',
           parse: '60',
           discount_price: '300'
         }, {
           text: '',
           src: 'http://odwbo6hwu.bkt.clouddn.com/webapp/img/list/fuke/fuke_02.jpg',
-          url: '',
+          url: '/pages/detail/detail?list=women&project=women6',
           parse: '100',
           discount_price: '340'
         }, {
           text: '',
           src: 'http://odwbo6hwu.bkt.clouddn.com/webapp/img/list/fuke/fuke_03.jpg',
-          url: '',
+          url: '/pages/detail/detail?list=women&project=women1',
           parse: '100',
           discount_price: '200'
         }, {
           text: '',
           src: 'http://odwbo6hwu.bkt.clouddn.com/webapp/img/list/fuke/fuke_04.jpg',
-          url: '',
+          url: '/pages/detail/detail?list=women&project=women2',
           parse: '280',
           discount_price: '562'
         }, {
           text: '',
           src: 'http://odwbo6hwu.bkt.clouddn.com/webapp/img/list/fuke/fuke_05.jpg',
-          url: '',
+          url: '/pages/detail/detail?list=women&project=women3',
           parse: '320',
           discount_price: '648'
         }, {
           text: '',
           src: 'http://odwbo6hwu.bkt.clouddn.com/webapp/img/list/fuke/fuke_06.jpg',
-          url: '',
+          url: '/pages/detail/detail?list=women&project=women4',
           parse: '420',
           discount_price: '848'
         }
@@ -152,25 +152,25 @@ Page({
         {
           text: '',
           src: 'http://odwbo6hwu.bkt.clouddn.com/webapp/img/list/nanke/nanke_01.jpg',
-          url: '',
+          url: '/pages/detail/detail?list=man&project=man1',
           parse: '70',
           discount_price: '140'
         }, {
           text: '',
           src: 'http://odwbo6hwu.bkt.clouddn.com/webapp/img/list/nanke/nanke_02.jpg',
-          url: '',
+          url: '/pages/detail/detail?list=man&project=man2',
           parse: '240',
           discount_price: '482'
         },{
           text: '',
           src: 'http://odwbo6hwu.bkt.clouddn.com/webapp/img/list/nanke/nanke_03.jpg',
-          url: '',
+          url: '/pages/detail/detail?list=man&project=man3',
           parse: '320',
           discount_price: '648'
         },{
           text: '',
           src: 'http://odwbo6hwu.bkt.clouddn.com/webapp/img/list/nanke/nanke_04.jpg',
-          url: '',
+          url: '/pages/detail/detail?list=man&project=man4',
           parse: '420',
           discount_price: '848'
         }
